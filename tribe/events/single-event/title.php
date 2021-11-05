7df32935dece4dc8e64900534e8da15f574c4e2b
Jenn Briden <jenn.allen@gmail.com>
2021-11-05 14:54:23 -0400

    removed the title from within the body of the event

---
+++ tribe/events/single-event/title.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Single Event Title Template Part
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/single-event/title.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.7.2
+ *
+ */
+?>
+
+// Removing the title from within the event so that it only shows up in the title of the page
+
+/**
+* <?php the_title( '<h1 class="tribe-events-single-event-title">', '</h1>' );
+*/
